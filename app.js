e6ed4d6c43bc0b6074531efb89553e5a0fb89952
McKade Hermansen <mckade.hermansen@gmail.com>
2016-12-06 11:22:47 -0700

    simplified app to get running on heroku

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express    = require('express');
 var app        = express();
 var bodyParser = require('body-parser');
 var engine     = require('ejs-mate');
-var pgp        = require('pg-promise')();
-var db         = pgp('postgres://localhost:5432/generator');
+//var pgp        = require('pg-promise')();
+//var db         = pgp('postgres://localhost:5432/generator');
 var path       = require('path');
 
 app.engine('ejs', engine);
@@ -21,22 +21,21 @@ app.get('/', function(req, res, next){
 });
 app.get('/signup', function(req, res, next){
   res.render('signup');
-})
-
-app.post('/loginForm', function(req, res, next){
-
 });
-app.post('/users/new', function(req, res, next){
-  db.none('insert into users(username, password, highscore)' +
-    'select ${username}, ${password}, ${highscore}' +
-    'where not exists(select 1 from users where username=${username})', req.body)
-    .then(function(){
-      res.redirect('/game');
-    })
-    .catch(function(err){
-      return next(err);
-    });
-});
+
+// app.post('/loginForm', function(req, res, next){
+//
+// });
+// app.post('/new', function(req, res, next){
+//   db.none('insert into posts(username, password, highscore)' +
+//     'values(${username}, ${password}, ${highscore})', req.body)
+//     .then(function(){
+//       res.redirect('/game');
+//     })
+//     .catch(function(err){
+//       return next(err);
+//     });
+// });
 
 app.listen(3000, function(){
   console.log('application running at localhost on port 3000');
